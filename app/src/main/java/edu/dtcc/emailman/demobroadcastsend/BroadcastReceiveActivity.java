de4300fd1cc6e40d6196cf33ccd2c161f95036d8
emailman <emailman@dtcc.edu>
2016-02-14 10:42:55 -0500

    Initial commit

---
+++ app/src/main/java/edu/dtcc/emailman/demobroadcastsend/BroadcastReceiveActivity.java
@@ -0,0 +1,17 @@
+package edu.dtcc.emailman.demobroadcastsend;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BroadcastReceiveActivity extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String actionName = intent.getAction();
+        if (actionName != null &&
+                actionName.equals("edu.dtcc.emailman.demo.testingbroadcast"));
+        String msg = intent.getStringExtra("MyMessage");
+        Log.d(" Eric ", msg);
+    }
+}
---
+++ app/src/main/java/edu/dtcc/emailman/demobroadcastsend/BroadcastSendMainActivity.java
@@ -0,0 +1,39 @@
+package edu.dtcc.emailman.demobroadcastsend;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class BroadcastSendMainActivity extends ActionBarActivity {
+    public static String BROADCAST_STRING = "edu.dtcc.emailman.demo.testingbroadcast";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast_send_main);
+
+        Button broadcastButton =
+                (Button) this.findViewById(R.id.broadcast_button);
+        broadcastButton.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                Intent broadcastIntent = new Intent();
+                broadcastIntent.putExtra
+                        ("MyMessage", "Time for you to do your own thing");
+                broadcastIntent.setAction(BROADCAST_STRING);
+                sendBroadcast(broadcastIntent);
+                Log.d("Eric", "requested intent");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_broadcast_send_main, menu);
+        return true;
+    }
+}
